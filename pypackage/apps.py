59b60672c29402c0721e56bb12ac0bbfc3418645
Grant Viklund <grant.viklund@gmail.com>
2019-05-24 12:04:20 -0700

    Initial commit

---
+++ pypackage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PypackageConfig(AppConfig):
+    name = 'pypackage'
---
+++ setup.py
@@ -0,0 +1,50 @@
+# --------------------------------------------
+# Copyright 2019, Grant Viklund
+# @Author: Grant Viklund
+# @Date:   2019-1-22 13:50:49
+# --------------------------------------------
+
+from os import path
+from setuptools import setup, find_packages
+
+file_path = path.abspath(path.dirname(__file__))
+with open(path.join(file_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+package_metadata = {
+    'name': 'django-pypackage',
+    'version': '0.0.3',
+    'description': 'Tools for simple hosting of Python packages.',
+    'long_description': long_description,
+    'url': 'https://github.com/renderbox/django-react-tools',
+    'author': 'Grant Viklund',
+    'author_email': 'renderbox@gmail.com',
+    'license': '',
+    'classifiers': [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+}
+
+setup(
+    **package_metadata,
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "Django>=2.1,<2.2",
+        ],
+    extras_require={
+        'dev': [],
+        'test': [],
+        'prod': [],
+        'build': ['wheel'],
+        'docs': [
+            'coverage==4.4.1',
+            'Sphinx==1.6.4'],
+    }
+)
