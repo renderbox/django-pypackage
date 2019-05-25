b357c09b6fa7c8a80f603ae329ce235637ffe20b
Grant Viklund <grant.viklund@gmail.com>
2019-05-24 17:59:00 -0700

    Added package ownership renamed remote_path to mirror_path in Version model added field for python version

--- pypackage/admin.py
+++ pypackage/admin.py
@@ -3,9 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from pypackage.models import Package, Version
 
+def recreate_slug(modeladmin, request, queryset):
+    '''
+    This uses the save() instead of update() since the AutoSlugField is only generated on save().
+    '''
+    for item in queryset.all():
+        item.slug = ""
+        item.save()
+
+recreate_slug.short_description = "Regenerate the Slug Field"
+
 
 class PackageAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields=('slug',)
+    actions = [recreate_slug]
 
 
 class VersionAdmin(admin.ModelAdmin):
